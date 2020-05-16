9616aa7ccbd7f7f4cd4c604577b6f22c765baebc
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2020-05-16 15:35:10 +0900

    メインのカレンダーコンテンツのVueコンポーネント化を解除（DOMが更新されないエラーが発生するため）

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import mainHeader from './components/mainHeader'
 import mainSide from './components/mainSide'
-import mainContent from './components/mainContent'
 
 const app = new Vue({
   el: '#app',
   components: {
     mainHeader,
     mainSide,
-    mainContent,
   }
 })
--- resources/views/include/main.blade.php
+++ resources/views/include/main.blade.php
@@ -1,1 +1,37 @@
-<main-content></main-content>
+<div class="main">
+  <div class="calendar-head">
+    <div class="calendar-head-left"><a href="#">前月へ</a></div>
+    <div class="calendar-head-center">2020年5月</div>
+    <div class="calendar-head-right"><a href="#">前月へ</a></div>
+  </div>
+  <div class="calendar-content">
+    <div class="calendar-content-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th>月</th>
+            <th>火</th>
+            <th>水</th>
+            <th>木</th>
+            <th>金</th>
+            <th>土</th>
+            <th>日</th>
+          </tr>
+        </thead>
+        <tbody>
+          @php
+          $date = 1;
+          for($i = 0;$i < 6;$i++) {
+            echo '<tr>';
+              for($j = 0;$j < 7;$j++) {
+                echo '<td>' . $date . '</td>';
+                $date++;
+              }
+            echo '</tr>';
+          }
+          @endphp
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
