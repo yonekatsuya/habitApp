71f81da1f7bb7f6232d49b88e60b353d206cdf02
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2020-05-24 14:02:17 +0900

    習慣チェックモーダルのラジオボタンのvalue値に項目のhabit_postsテーブルのid値も用意した。

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import mainHeader from './components/mainHeader'
 import mainSide31 from './components/mainSide31'
-import mainContent159 from './components/mainContent159'
+import mainContent160 from './components/mainContent160'
 
 const app = new Vue({
   el: '#app',
   components: {
     mainHeader,
     mainSide31,
-    mainContent159,
+    mainContent160,
   }
 })
