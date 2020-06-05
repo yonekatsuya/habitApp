8acd05014343e6b08696e8adca18aa2a1a1a631a
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2020-06-05 14:24:59 +0900

    toBeforeMonthメソッドとtoNextMonthメソッドの共通化

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import mainHeader1 from './components/mainHeader1'
 import mainSide233 from './components/mainSide233'
-import mainContent198 from './components/mainContent198'
+import mainContent199 from './components/mainContent199'
 
 const app = new Vue({
   el: '#app',
   components: {
     mainHeader1,
     mainSide233,
-    mainContent198,
+    mainContent199,
   }
 })
